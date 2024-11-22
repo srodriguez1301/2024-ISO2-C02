3aabb7dcb8878b95f1d8c4438751268f303c0cae
alvaromozos12 <alvaro.mozos@alu.uclm.es>
2024-11-22 13:48:00 +0100

    Creación servidor cliente en main

---
+++ src/servidor/mapear-informacion/src/main/java/org/mapear/informacion/App.java
@@ -0,0 +1,10 @@
+package org.mapear.informacion;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
