d5e5519c14229734d20ed5d1bdb1755a55bc0081
alvaromozos12 <alvaro.mozos@alu.uclm.es>
2024-11-23 17:54:40 +0100

    Crear modulo maven

---
+++ src/cliente/mapear-infomacion/src/main/java/es/uclm/esi/mapear_informacion/App.java
@@ -0,0 +1,10 @@
+package es.uclm.esi.mapear_informacion;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
