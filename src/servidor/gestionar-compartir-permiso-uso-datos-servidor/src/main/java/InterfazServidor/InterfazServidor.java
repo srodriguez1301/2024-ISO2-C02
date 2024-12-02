4e64070ec40a09c4126b5e485c9dea1e5b19860e
Steven TA <steven.tipantuna@alu.uclm.es>
2024-12-03 00:38:46 +0100

    Añadiendo las carpetas InterfazServidor e InterfazUsuario al módulo del servidor

---
+++ src/servidor/gestionar-compartir-permiso-uso-datos-servidor/src/main/java/InterfazServidor/InterfazServidor.java
@@ -0,0 +1,27 @@
+package ServidorIT3.InterfazServidor;
+
+import Cliente.Dominio.Project;
+import Cliente.Dominio.Task;
+
+public class InterfazServidor {
+
+	public boolean procesarProyecto(Project aProyecto) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Project validarProyecto(Project aProyecto) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean almacenarProyecto(Project aProyecto) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean procesarTareas(Task aTarea) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void responder(Project aProyecto) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/servidor/gestionar-compartir-permiso-uso-datos-servidor/src/main/java/InterfazUsuario/InterfazUsuario.java
@@ -0,0 +1,10 @@
+package ServidorIT3.InterfazUsuario;
+
+import Cliente.Dominio.Project;
+
+public class InterfazUsuario {
+
+	public boolean EnviarProyecto(Project aProject) {
+		throw new UnsupportedOperationException();
+	}
+}
