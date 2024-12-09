58981a1b87d66e8e0a0a9238abd2c8e23e7777c9
Steven TA <steven.tipantuna@alu.uclm.es>
2024-12-09 23:17:37 +0100

    Añadiendo implementacion it4

---
+++ src/servidor/src/main/java/InterfazUsuario/ValidadorLocal.java
@@ -0,0 +1,10 @@
+package InterfazUsuario;
+
+import Dominio.Dominio;
+
+public class ValidadorLocal {
+
+	public boolean validarDatos(Dominio aDominio) {
+		throw new UnsupportedOperationException();
+	}
+}
