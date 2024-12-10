c12a5b3020438b7c9327f9cd97bf59df74c40824
Steven TA <steven.tipantuna@alu.uclm.es>
2024-12-10 21:56:07 +0100

    Añadiendo implementación de cliente

---
+++ src/cliente/definir-dominio-datos-cliente/src/main/java/InterCIServidor/InterfazCrearServidor.java
@@ -0,0 +1,22 @@
+package InterCIServidor;
+
+import Dominio.Dominio;
+
+public class InterfazCrearServidor {
+
+	public void controlDominio(Dominio aDominio) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void validarDominio(Dominio aDominio) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void asignarDominio(Dominio aDominio) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void proporcionarInformacion(Dominio aDominio) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/cliente/definir-dominio-datos-cliente/src/main/java/InterfazUsuario/InterfazCrearUsuario.java
@@ -0,0 +1,10 @@
+package InterfazUsuario;
+
+import Dominio.Dominio;
+
+public class InterfazCrearUsuario {
+
+	public void enviarDominio(Dominio aDominio) {
+		throw new UnsupportedOperationException();
+	}
+}
