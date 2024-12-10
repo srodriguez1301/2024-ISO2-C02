6b3feec3334d41a47a4cb4c9c608a185db0b13d0
alvaromozos12 <alvaro.mozos@alu.uclm.es>
2024-12-10 15:43:20 +0100

    Añadir submodulo

---
+++ src/cliente/definir-dominio-datos-cliente/src/main/java/es/uclm/esi/datos/cliente/App.java
@@ -0,0 +1,10 @@
+package es.uclm.esi.datos.cliente;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
