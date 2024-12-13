7a54f26ef3b380db156aa7d4fb0ca5d3bdb36e63
alvaromozos12 <alvaro.mozos@alu.uclm.es>
2024-12-13 20:06:58 +0100

    Componente It6 cliente

---
+++ src/cliente/documentar-procesos-cliente/src/main/java/es/uclm/esi/documentar/procesos/cliente/App.java
@@ -0,0 +1,10 @@
+package es.uclm.esi.documentar.procesos.cliente;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
