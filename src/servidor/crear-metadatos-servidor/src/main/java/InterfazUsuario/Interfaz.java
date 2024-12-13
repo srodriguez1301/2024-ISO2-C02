2fa5de12f234b62ed4ff7d188b31f4971f252c4a
Steven TA <steven.tipantuna@alu.uclm.es>
2024-12-13 15:44:40 +0100

    Implementación Servidor Iteración 5

---
+++ src/servidor/crear-metadatos-servidor/src/main/java/InterfazUsuario/Interfaz.java
@@ -0,0 +1,16 @@
+package InterfazUsuario;
+
+public class Interfaz {
+
+	public Metadatos solicitarDatos() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarErroresValidacion(List<String> aMensajes) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarVistaPrevia(Metadatos aMetadatos) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/servidor/crear-metadatos-servidor/src/main/java/InterfazUsuario/InterfazCrearUsuario.java
@@ -0,0 +1,15 @@
+package InterfazUsuario;
+
+import Dominio.Usuario;
+import Dominio.Metadato;
+
+public class InterfazCrearUsuario {
+
+	public void enviarUsuario(Usuario aUsuario) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void enviarMetadato(Metadato aMetadato) {
+		throw new UnsupportedOperationException();
+	}
+}
