d907f42f44d2c0905ca1b12ab954ad7ce173bbba
alvaromozos12 <alvaro.mozos@alu.uclm.es>
2024-12-13 20:05:09 +0100

    Componente It6 servidor

---
+++ src/servidor/documentar-procesos-servidor/src/test/java/es/ulcm/esi/documentar/procesos/servidor/AppTest.java
@@ -0,0 +1,19 @@
+package es.ulcm.esi.documentar.procesos.servidor;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
